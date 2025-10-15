8f885fca7f95b302e04d64ab07a55a01f2a65e77
amandagalligan <ephemerism@gmail.com>
2025-10-15 12:54:48 +0100

    test: add additional test cases for isPrime and implement prompt output test

--- main_test.go
+++ main_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"os"
+	"testing"
+)
 
 func Test_isPrime(t *testing.T) {
 	primeTests := []struct {
@@ -11,6 +15,9 @@ func Test_isPrime(t *testing.T) {
 	}{
 		{"prime", 7, true, "7 is a prime number!"},
 		{"not prime", 8, false, "8 is not a prime number because it is divisible by 2!"},
+		{"zero", 0, false, "0 is not prime, by definition!"},
+		{"one", 1, false, "1 is not prime, by definition!"},
+		{"negative number", -11, false, "Negative numbers are not prime, by definition!"},
 	}
 
 	for _, e := range primeTests {
@@ -26,5 +33,32 @@ func Test_isPrime(t *testing.T) {
 		if e.msg != msg {
 			t.Errorf("%s: expected %s but got %s", e.name, e.msg, msg)
 		}
+	}
+}
+
+func Test_prompt(t *testing.T) {
+	// save a copy of os.Stdout
+	oldOut := os.Stdout
+
+	// create a read and write pipe
+	r, w, _ := os.Pipe()
+
+	// set os.Stdout to our write pipe
+	os.Stdout = w
+
+	prompt()
+
+	// close our writer
+	_ = w.Close()
+
+	// reset os.Stdout to what it was before
+	os.Stdout = oldOut
+
+	// read the output of our prompt() func from our read pipe
+	out, _ := io.ReadAll(r)
+
+	// perform our test
+	if string(out) != "-> " {
+		t.Errorf("incorrect prompt: expected -> but got %s", string(out))
 	}
 }
